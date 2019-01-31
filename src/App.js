c8e5723384741a3f116f30af8171ced3ce025f88
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-01-31 17:19:24 +0100

    Add placeholder functionality to text input
    Be able to read what is added

--- src/App.js
+++ src/App.js
@@ -20,7 +20,20 @@ const particleOptions = {
 }
 }
 
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +43,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/*
         
         <FaceRecognition />*/}
