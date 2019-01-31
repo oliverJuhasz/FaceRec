df6007931f497dfb6cbc04e47df00f13258bac8b
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-01-31 15:39:13 +0100

    Correcting import error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from "./components/navigation/navigation";
+import Logo from "./components/logo/logo";
 import "tachyons";
+import Tilt from "react-tilt";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/*<Logo />
+        <Logo />
+        {/*
         <ImageLinkForm />
         <FaceRecognition />*/}
       </div>
