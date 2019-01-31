e343e91494b33df8ed988dc5a32c0fbd4795825d
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-01-31 15:53:25 +0100

    Remove dead link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navigation from "./components/navigation/navigation";
 import Logo from "./components/logo/logo";
 import "tachyons";
-import Tilt from "react-tilt";
 
 class App extends Component {
   render() {
