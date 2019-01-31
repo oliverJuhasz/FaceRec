99010137eb497b81415a76c6d020fe022e41948f
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-01-31 15:05:39 +0100

    Import navigation, clear imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./component/Navigation/Navigation";
 
 class App extends Component {
   render() {
