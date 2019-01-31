15436d42d45f1b545514892b13f5d94e5654f4b9
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-01-31 17:11:10 +0100

    Install and import react-particles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Logo from "./components/logo/logo";
 import "tachyons";
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+
+    }
+  }
+}
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles"
+          params={particleOptions}
+          />
         <Navigation />
         <Logo />
         <Rank />
