228ca94853cf18722e8a155f9abd1e1dd1d589c1
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-03 21:09:36 +0100

    Add mock database and signin

--- _backend/server.js
+++ _backend/server.js
@@ -1,13 +1,42 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {id: "123",
+        name: "john",
+        email: "john@gmail.com",
+        password: "cookies",
+        entries: 0,
+        joined: new Date()},
+        {id: "1234",
+        name: "sally",
+        email: "sally@gmail.com",
+        password: "bananas",
+        entries: 0,
+        joined: new Date()}    
+]
+};
 
 app.get("/", (req, res) => {
     res.send("this is working")
-})
+});
+
+app.post("/signin", (req, res) => {
+    if (req.body.email === database.users[0].email 
+    && req.body.password === database.users[0].password) {
+        res.json("success");
+    } else {
+        res.status(400).json("error logging in")
+    }
+});
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
-})
+});
 
 
 /*
