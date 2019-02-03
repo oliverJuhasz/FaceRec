43abe78065fbc7cbda6f4a088d68f2779dc136bc
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-03 21:27:36 +0100

    Add basic register functionality

--- _backend/server.js
+++ _backend/server.js
@@ -22,7 +22,7 @@ const database = {
 };
 
 app.get("/", (req, res) => {
-    res.send("this is working")
+    res.send(database.users)
 });
 
 app.post("/signin", (req, res) => {
@@ -33,6 +33,19 @@ app.post("/signin", (req, res) => {
         res.status(400).json("error logging in")
     }
 });
+
+app.post("/register", (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: "125",
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()}
+    )
+    res.json(database.users[database.users.length-1]);
+})
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
