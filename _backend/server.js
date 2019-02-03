cf33371a557a3630413112648efcf3b24b7bea7f
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-03 19:37:05 +0100

    Plan required features

--- _backend/server.js
+++ _backend/server.js
@@ -8,3 +8,12 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("app is running on port 3000");
 })
+
+
+/*
+/ --> res = this is working
+/signin --> POST success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
