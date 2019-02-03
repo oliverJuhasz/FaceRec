b3c65d7fdff305f835b9ba93aab086f7aaf2caf6
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-04 00:26:03 +0100

    Add loaduser function to main app

--- src/App.js
+++ src/App.js
@@ -37,8 +37,25 @@ class App extends Component {
       box: {},
       route: "home",
       isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "john@.com",
+        entries: 0,
+        joined: ""
+      }
     }
   };
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -105,7 +122,7 @@ class App extends Component {
           : (
             route === "signin" 
             ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )}
       
     </div>); 
