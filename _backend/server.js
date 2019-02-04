bebb6dd64f34787fcaf36d5ebe2f7c78d422d37c
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-04 21:11:22 +0100

    Replace static database with SQL in profile function

--- _backend/server.js
+++ _backend/server.js
@@ -61,21 +61,23 @@ app.post("/register", (req, res) => {
     }).then(user => {
         res.json(user[0])
     })
-    .catch(err => res.status(400).json(err))
+    .catch(err => res.status(400).json("unable to register"))
     });
 
 app.get("/profile/:id", (req, res) => {
     const { id } = req.params;
-    let found = false
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
-    if (!found) {
+    db.select("*").from("users").where("id", id)
+    .then(user => {
+        if (user.length) {
+        res.json(user[0])
+    } else {
         res.status(400).json("not found")
     }
+
+    }
+    )
+    .catch(err => res.status(400).json("error getting user"))
+
     }
 );
 
