60e676a44d375cff76a016fa953bc96ffc08837c
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-04 01:33:57 +0100

    Fix if within onsubmitsignin. Minor refactoring of HTML

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -28,8 +28,8 @@ class Signin extends React.Component {
         })
         .then(response => response.json())
         .then(user => {
-            if (user) {
-                this.props.loadUser(user)
+            if (user.id) {
+                this.props.loadUser(user);
                 this.props.onRouteChange("home");
             }
         })
@@ -40,7 +40,7 @@ class Signin extends React.Component {
         return (
                 <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                     <main className="pa4 black-80">
-                        <form className="measure">
+                        <div className="measure">
                             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                                 <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                                 <div className="mt3">
@@ -48,14 +48,15 @@ class Signin extends React.Component {
                                     <input 
                                     onChange={this.onEmailChange} 
                                     className="pa2 input-reset ba bg-transparent  w-100" 
-                                    type="email" name="email-address"  
+                                    type="email" 
+                                    name="email-address"  
                                     id="email-address" 
                                     />
                                 </div>
                                 <div className="mv3">
                                     <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                     <input 
-                                    onChange={this.onPasswordChange}
+                                    onChange={this.onPasswordChange} 
                                     className="b pa2 input-reset ba bg-transparent  w-100" 
                                     type="password" 
                                     name="password"  
@@ -68,12 +69,13 @@ class Signin extends React.Component {
                                 onClick={this.onSubmitSignIn}
                                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                                 type="submit" 
-                                value="Sign in" />
+                                value="Sign in" 
+                                />
                             </div>
                             <div className="lh-copy mt3">
-                                <a onClick={() => onRouteChange("register")} href="#0" className="f6 link dim black db pointer">Register</a>
+                                <p onClick={() => onRouteChange("register")} href="#0" className="f6 link dim black db pointer">Register</p>
                             </div>
-                            </form>
+                            </div>
                         </main>
                     </article>    );
     }
