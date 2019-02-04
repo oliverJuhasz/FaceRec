473064b899941c49faf6db1d2d6623580450e6df
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-04 23:04:30 +0100

    Add error logging to onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -114,7 +114,9 @@ class App extends Component {
     .then(count => {
       this.setState(Object.assign(this.state.user, { entries: count }))
     })
-    }
+    .catch(console.log)
+  }
+ 
       this.displayFaceBox(this.calculateFaceLocation(response))
     .catch(err =>console.log(err))
   })}
