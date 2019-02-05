1e32009f6981d16dbf88d5956c476c3f4eba27ff
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-05 17:27:05 +0100

    Fix navbar bug
    Navbar didn't change upon logging out

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Navigation = ( {onRouteChange, isSignedIn } ) => {
-        if (isSignedIn) {
+const Navigation = ( {onRouteChange, isSignedIn } ) => {  
+    if (isSignedIn) {
             return (
         <nav style ={{display: "flex", justifyContent: "flex-end"}} >
-            <p onClick ={() => onRouteChange("signin")}   className="f3 link dim black underline pa3 pointer ">Sign Out</p>
+            <p onClick ={() => onRouteChange("signout")}   className="f3 link dim black underline pa3 pointer ">Sign Out</p>
         </nav>
         )} else {
             return (
